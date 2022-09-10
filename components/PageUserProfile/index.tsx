9389de371bcf323180bd57c819c7f24957b541b3
Anirudha Bose <anirudha.bose@alumni.cern>
2022-09-11 04:50:54 +0530

    Fetch domain owner from name service registry

--- components/PageUserProfile/index.tsx
+++ components/PageUserProfile/index.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from 'react'
-import { getDomainKey } from '@bonfida/spl-name-service'
+import { useEffect, useMemo, useState } from 'react'
+import { getDomainKey, NameRegistryState } from '@bonfida/spl-name-service'
+import { Connection } from '@solana/web3.js'
 import cn from 'classnames'
+import axios from 'axios'
 
 import Main from './Main'
 import Catalog from '../Catalog'
 import styles from './PageUserProfile.module.sass'
-import axios from 'axios'
 import { NFT, Trait, Transaction } from '../../types'
 
 type UserProfileProps = {
@@ -16,47 +17,56 @@ const UserProfile = (props: UserProfileProps) => {
   const [address, setAddress] = useState<string>('')
   const [assets, setAssets] = useState<NFT[]>([])
 
+  const connection = useMemo(() => {
+    return new Connection('https://api.mainnet-beta.solana.com')
+  }, [])
+
   useEffect(() => {
     ;(async () => {
-      const { pubkey } = await getDomainKey(props.domain)
+      if (!props.domain) {
+        return
+      }
 
-      const addr = pubkey.toBase58()
+      const { pubkey } = await getDomainKey(props.domain)
+      const {
+        registry: { owner: wallet },
+      } = await NameRegistryState.retrieve(connection, pubkey)
+
+      const addr = wallet.toBase58()
       setAddress(addr)
 
-      axios
-        .post(`/api/nfts?address=8eekKfUAGSJbq3CdA2TmHb8tKuyzd5gtEas3MYAtXzrT`)
-        .then((response) => {
-          const assets = response.data.result.assets
+      axios.post(`/api/nfts?address=${addr}`).then((response) => {
+        const assets = response.data.result.assets
 
-          setAssets(
-            assets.map(
-              (asset: any) =>
-                ({
-                  name: asset.name,
-                  description: asset.description,
-                  collection: asset.collectionName,
-                  image: asset.imageUrl,
-                  traits: asset.traits.map(
-                    (trait: any) =>
-                      ({
-                        type: trait.trait_type,
-                        value: trait.value,
-                      } as Trait)
-                  ),
-                  provenance: asset.provenance.map(
-                    (txn: any) =>
-                      ({
-                        date: txn.date,
-                        blockNumber: txn.blockNumber,
-                        hash: txn.txHash,
-                      } as Transaction)
-                  ),
-                } as NFT)
-            )
+        setAssets(
+          assets.map(
+            (asset: any) =>
+              ({
+                name: asset.name,
+                description: asset.description,
+                collection: asset.collectionName,
+                image: asset.imageUrl,
+                traits: asset.traits.map(
+                  (trait: any) =>
+                    ({
+                      type: trait.trait_type,
+                      value: trait.value,
+                    } as Trait)
+                ),
+                provenance: asset.provenance.map(
+                  (txn: any) =>
+                    ({
+                      date: txn.date,
+                      blockNumber: txn.blockNumber,
+                      hash: txn.txHash,
+                    } as Transaction)
+                ),
+              } as NFT)
           )
-        })
+        )
+      })
     })()
-  }, [props.domain])
+  }, [connection, props.domain])
 
   return (
     <div className={cn('section-main', styles.section)}>
